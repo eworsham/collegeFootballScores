acf9bd140bd3a6fdc4f939753592b5facc0f2999
eworsham <eworsham64@gmail.com>
2024-10-26 23:28:38 -0400

    Added form for score prediction

--- js/GameDetails.mjs
+++ js/GameDetails.mjs
@@ -1,15 +1,33 @@
 function buildGameCard(game) {
-    
-    console.log(game)
-
-
-    const gameCard = document.createElement('div')
+    let gameCard = document.createElement('div')
     gameCard.setAttribute('class', 'gameCard')
     gameCard.innerHTML = `
-        <p>${game.home_team}</p>
-        <p>${game.away_team}</p>
+        <p class="teamName">${game.home_team}</p>
+        <p class="teamName">${game.away_team}</p>
         <p class="score">${game.home_points ? game.home_points : 0} : ${game.away_points ? game.away_points : 0}</p>
     `
+
+    // Add prediction form if game hasn't started yet
+    const today = new Date()
+    const gameStart = new Date(game.start_date)
+    if (today < gameStart) {
+        gameCard.innerHTML += `
+            <form class="scorePrediction">
+                <div>
+                    <label for="homeScore">Home Score: </label>
+                    <input type="number" name="homeScore" id="homeScore" required>                
+                </div>
+
+                <div>
+                    <label for="awayScore">Away Score: </label>
+                    <input type="number" name="awayScore" id="awayScore">
+                </div>
+
+                <button type="button">Predict Score</button>
+            </form>
+        `
+    }
+
     return gameCard
 }
 
