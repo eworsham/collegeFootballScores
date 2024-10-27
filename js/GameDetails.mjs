0bed8de132c9cd79cea6b4632946f6262647208f
eworsham <eworsham64@gmail.com>
2024-10-27 01:23:27 -0400

    Finished predictions and games view

--- js/GameDetails.mjs
+++ js/GameDetails.mjs
@@ -2,9 +2,9 @@ function buildGameCard(game) {
     let gameCard = document.createElement('div')
     gameCard.setAttribute('class', 'gameCard')
     gameCard.innerHTML = `
-        <p class="teamName">${game.home_team}</p>
         <p class="teamName">${game.away_team}</p>
-        <p class="score">${game.home_points ? game.home_points : 0} : ${game.away_points ? game.away_points : 0}</p>
+        <p class="teamName">${game.home_team}</p>
+        <p class="score">${game.away_points ? game.away_points : 0} : ${game.home_points ? game.home_points : 0}</p>
     `
 
     // Add prediction form if game hasn't started yet
@@ -13,17 +13,21 @@ function buildGameCard(game) {
     if (today < gameStart) {
         gameCard.innerHTML += `
             <form class="scorePrediction">
-                <div>
-                    <label for="homeScore">Home Score: </label>
-                    <input type="number" name="homeScore" id="homeScore" required>                
-                </div>
-
+                <h3>Prediction</h3>
                 <div>
                     <label for="awayScore">Away Score: </label>
-                    <input type="number" name="awayScore" id="awayScore">
+                    <input type="number" name="awayScore" id="awayScore${game.id}">
                 </div>
 
-                <button type="button">Predict Score</button>
+                <div>
+                    <label for="homeScore">Home Score: </label>
+                    <input type="number" name="homeScore" id="homeScore${game.id}" required>                
+                </div>
+
+                <div class="predictionButton">
+                    <p>You can only place one prediction</p>
+                    <button type="button" id="predictScoreButton-${game.id}">Predict Score</button>
+                </div>
             </form>
         `
     }
@@ -48,6 +52,45 @@ export default class GameDetails {
         games.innerHTML = '';
         this.gamesData.forEach(game => {
             games.appendChild(buildGameCard(game))
+
+            const predictScoreButton = document.querySelector(`#predictScoreButton-${game.id}`)
+            if (predictScoreButton) {
+                predictScoreButton.addEventListener('click', () => this.addPrediction(game))
+            }
+
+            this.getPrediction(game)
         });
+    }
+
+    addPrediction(game) {
+        const scores = JSON.parse(localStorage.getItem('scores')) || []
+
+        const scoreItem = {
+            id: game.id,
+            away_score: document.querySelector(`#awayScore${game.id}`).value,
+            home_score: document.querySelector(`#homeScore${game.id}`).value
+        }
+
+        scores.push(scoreItem)
+        localStorage.setItem(`scores`, JSON.stringify(scores))
+
+        document
+            .querySelector(`#predictScoreButton-${game.id}`)
+            .classList.add('hidden')
+    }
+
+    getPrediction(game) {
+        const scores = JSON.parse(localStorage.getItem('scores')) || []
+
+        scores.forEach(score => {
+            if (score.id === game.id) {
+                document.querySelector(`#awayScore${game.id}`).value = score.away_score
+                document.querySelector(`#homeScore${game.id}`).value = score.home_score
+
+                document
+                    .querySelector(`#predictScoreButton-${game.id}`)
+                    .classList.add('hidden')
+            }
+        })
     }
 }
