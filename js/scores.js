5227de00f574aeb071e53daf9098acceeb01f345
eworsham <eworsham64@gmail.com>
2024-10-26 20:10:39 -0400

    Fixed function call for getScores

--- js/scores.js
+++ js/scores.js
@@ -2,7 +2,7 @@ import ExternalServices from "./externalServices.mjs";
 import ScoreDetails from "./ScoreDetails.mjs";
 
 const externalServices = new ExternalServices()
-const dataSource = await externalServices.getData()
+const dataSource = await externalServices.getScores()
 
 const scoreDetails = new ScoreDetails(dataSource)
 
